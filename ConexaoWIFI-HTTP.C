e6145032ed6a826491d2b8d699b21ebd31a65a9f
ALBERTHO SÍZINEY COSTA <53822577+alberthosc@users.noreply.github.com>
2022-01-25 18:27:36 -0300

    Create ConexaoWIFI-HTTP.C

---
+++ ConexaoWIFI-HTTP.C
@@ -0,0 +1,61 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+
+const char* login = "brisa-1776554";
+const char* password =  "5qabth84";
+
+WiFiServer servidor (80);
+
+void setup() 
+{
+  pinMode(23, OUTPUT);
+  
+  Serial.begin(9600);
+  WiFi.begin(login, password);
+  Serial.print("Connectando ao WiFi");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("\nConectado ao WiFi");
+  Serial.print("Endereço IP: ");
+  Serial.println(WiFi.localIP());
+
+  servidor.begin();
+}
+
+void loop() 
+{
+  WiFiClient client = servidor.available();
+
+  if (client) 
+  {
+    Serial.println("new client");
+    String currentLine = "";
+    while (client.connected()) 
+    {
+      if (client.available()) 
+      {
+        char c = client.read();
+        Serial.write(c);
+
+        if (c != '\r') 
+        {  
+          currentLine += c;    
+        }
+        if (currentLine.endsWith("GET /1")) 
+        {
+          digitalWrite(23, HIGH);          
+        }
+        else if (currentLine.endsWith("GET /0")) 
+        {
+          digitalWrite(23, LOW);          
+        }
+      }
+    }
+  }
+}
